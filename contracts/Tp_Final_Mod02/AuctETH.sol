93fe23ccb2add9aa75fb0972fb7b38f8ea44d0f0
Ivan Carneiro <ivanexequielc@gmail.com>
2024-11-02 18:33:03 -0300

    modifier auctionOpened() OK

--- contracts/Tp_Final_Mod02/AuctETH.sol
+++ contracts/Tp_Final_Mod02/AuctETH.sol
@@ -2,48 +2,60 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-/* CONSIGNAS
-    - INICIALIZACIÓN:
-        - Definir valor mín de inicio. *
-        - Definir fecha/hora inicio. *
-        - Definir duración de subasta. *
-        - Definir saltos mínimos entre pujas. *
-        - Definir tiempos de extensión por pujas nuevas.
+/// @title AuctETH - A smart contract for Ethereum-based auctions
+/// @notice This contract allows users to participate in a timed auction
+/// @dev Implements features like bid increments, automatic extensions, and refunds with commission
+/// @author Ivan Carneiro
 
-    - Ejecución Subasta
-        Minetras Subasta Activa...
-            BID --> ofertaNueva -->
-                If (ofertaNueva > precioActual){
-                    mejorOferta = ofertaNueva;
-                    mejorBidder = ultBidder;
-                    masTiempo(extiende el tiempo de la subasta);
-                }
-                Esle {
-                    "Oferta nueva desetimada.";
-                }
-
-        Asignación:
-            - Tiempo concluído == true.
-            - Transferir importe final al vendedor.
-            - Transferir premio al ganador.
-*/
-
-contract Bid {
-    
-    
+contract AuctETH {
+    address owner;
     uint256 initValue;
-    uint256 beginTimestamp;
+    uint256 currentBid;
+    uint8 bidIncrement;
     uint256 duration;
-    uint256 bidIncrement;
-    uint256 extTime;
     uint256 finishTimestamp;
 
-    constructor(uint256 _initValue, uint256 _beginTimestamp, uint256 _duration, uint256 _bidIncrement, uint256 _extTime){
+    /// @notice Constructor to initialize the auction.
+    /// @param _initValue The initial value of the auction item.
+    /// @param _bidIncrement The minimum bid increment (in percentage) e.g. 5% > to current bid.
+    /// @param _duration The duration of the auction (in seconds), e.g. (5min = 300sec; 10min = 600sec; 1h = 3600sec; 1d = 86400sec).
+    constructor(
+        uint256 _initValue,
+        uint8 _bidIncrement,
+        uint256 _duration
+    ) {
+        owner = msg.sender;
         initValue = _initValue;
-        beginTimestamp = _beginTimestamp;
-        duration = _duration;
+        currentBid = 0;
         bidIncrement = _bidIncrement;
-        extTime = _extTime;
-        finishTimestamp = block.timestamp + _duration;
+        duration = _duration;
+        finishTimestamp = block.timestamp + duration;
     }
+
+    // struct Bidder (address)
+    struct Bidder {
+        uint256 id;
+        address addrs;
+        uint256 bidValue;
+        uint256 bidderBalance;
+    }
+
+    // array bidders
+    Bidder[] public bidders;
+
+    event NewBid(address indexed addrs, uint256 _bidValue);
+
+    modifier auctionOpened() {
+        require(block.timestamp < finishTimestamp, "The auction has ended.");
+        _;
+    }
+    // ofertar
+    function makeBid() external auctionOpened {
+    }
+
+    // mostrar Ganador
+    // mostrar Ofertas
+    // devolver Depósitos
+    // REEMBOLSO PARCIAL
+    // EXTENSION DEL PLAZO
 }
---
+++ contracts/Tp_Final_Mod02/Tp_final_M02_consignas.md
@@ -0,0 +1,30 @@
+**Trabajo Práctito Final Módulo 2 (Auction Smart Contract)**
+===
+***
+
+
+## Se requiere un contrato inteligente verificado y publicado en la red de Scroll Sepolia que cumpla con lo siguiente:
+
+
+### **Funciones básicas**
+- **Constructor:** Inicializar la subasta con los parámetros necesarios.
+- **Ofertar:** Permitir que los participantes hagan una oferta válida (superior en al menos 5% a la actual) mientras la subasta esté activa.
+- **Mostrar ganador:** Mostrar el ganador y el monto de la oferta ganadora.
+- **Mostrar ofertas:** Listar todos los ofertantes y sus montos.
+- **Devolver depósitos:** Al finalizar, devolver los depósitos a quienes no ganaron (menos una comisión del 2% para el gas).
+
+
+### **Funcionalidades avanzadas**
+- **Reembolso parcial:** Los participantes pueden retirar el exceso de su última oferta.
+- **Extensión del plazo:** La subasta se extiende 10 minutos con cada nueva oferta válida en los últimos 10 minutos.
+
+
+### **Consideraciones importantes**
+- Usen modificadores donde sea adecuado.
+- Una vez finalizada la subasta el propietario del contrato ejecutará una función para devolver el depósito a los ofertantes que no ganaron y transferir el premio al ganador (El importe de la mejor oferta).  El propietario no interviene en la finalización de la subasta. La subasta tienen una duración predefinida y se puede extender en la medida en que sigan llegando nuevas ofertas.
+
+---
+*Asegúrense de manejar adecuadamente los errores y usar eventos para comunicar cambios de estado.*
+
+
+*Documentación clara y completa explicando funciones, variables y eventos.*
