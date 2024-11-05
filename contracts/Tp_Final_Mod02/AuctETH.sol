1faa60ef4160f21cb2e8367c05c560f3f2a4aa1e
ivancarneiro <ivanexequielc@gmail.com>
2024-11-04 22:49:54 -0300

    update contract

--- contracts/Tp_Final_Mod02/AuctETH.sol
+++ contracts/Tp_Final_Mod02/AuctETH.sol
@@ -11,51 +11,96 @@ contract AuctETH {
     address owner;
     uint256 initValue;
     uint256 currentBid;
-    uint8 bidIncrement;
+    uint256 bidIncrement = 2;
     uint256 duration;
     uint256 finishTimestamp;
 
     /// @notice Constructor to initialize the auction.
     /// @param _initValue The initial value of the auction item.
-    /// @param _bidIncrement The minimum bid increment (in percentage) e.g. 5% > to current bid.
+    /// @param _bidIncrement The minimum bid increment (in percentage), if not specified, will be 2%.
     /// @param _duration The duration of the auction (in seconds), e.g. (5min = 300sec; 10min = 600sec; 1h = 3600sec; 1d = 86400sec).
-    constructor(
-        uint256 _initValue,
-        uint8 _bidIncrement,
-        uint256 _duration
-    ) {
+    constructor(uint256 _initValue, uint256 _bidIncrement, uint256 _duration) {
+        require(_initValue > 0,"The initial value must be greater than zero for the gas");
         owner = msg.sender;
         initValue = _initValue;
-        currentBid = 0;
+        currentBid = _initValue;
         bidIncrement = _bidIncrement;
         duration = _duration;
         finishTimestamp = block.timestamp + duration;
     }
 
+    mapping(address => uint256) public offers;
+
     // struct Bidder (address)
     struct Bidder {
-        uint256 id;
         address addrs;
-        uint256 bidValue;
-        uint256 bidderBalance;
+        uint256 lastBid;
     }
 
-    // array bidders
-    Bidder[] public bidders;
+    Bidder[] public bidders; // array de todos los pujadores
 
-    event NewBid(address indexed addrs, uint256 _bidValue);
+    address[] public uniqueAddr; // array de direcciones unicas
 
-    modifier auctionOpened() {
-        require(block.timestamp < finishTimestamp, "The auction has ended.");
+
+    event NewBid(address indexed _bidder, uint256 _bidValue);
+
+    // Modifiers
+    modifier onlyOwner() {
+        require(msg.sender == owner,"Only the auction owner can execute this function");
         _;
     }
+
+    modifier auctionOpened() {
+        require(block.timestamp < finishTimestamp, "The auction has ended");
+        _;
+    }
+
+    modifier auctionFinished() {
+        require(block.timestamp >= finishTimestamp,"The auction has not yet ended");
+        _;
+    }
+
+
+    // mostrar oferta actual
+    function showCurrentBid() external view returns (uint256 _currentBid) {
+        _currentBid = currentBid;
+        return (_currentBid);
+    }
+
     // ofertar
-    function makeBid() external auctionOpened {
+    function makeBid() external payable auctionOpened {
+        address _bidder = msg.sender;
+        uint256 _bidValue = msg.value;
+
+        require(_bidValue > (currentBid * ((bidIncrement + 100) / 100)),"The bid must exceed the best offer, taking into account the minimum incremental value");
+        if (offers[_bidder]==0) {
+            uniqueAddr.push(_bidder);
+        }
+        offers[_bidder] += _bidValue;
+        currentBid = _bidValue;
+        bidders.push(Bidder(_bidder, _bidValue));
+        emit NewBid(_bidder, _bidValue);
+    }
+
+    // mostrar ofertas
+    function showBidders() external view returns (Bidder[] memory _bidders) {
+        uint256 len = uniqueAddr.length;
+        for (uint256 i=0; i<len; i++){
+            _bidders[i] = Bidder( uniqueAddr[i],offers[uniqueAddr[i]]);
+        }
+    }
+
+    // REEMBOLSO PARCIAL
+    // EXTENSION DEL PLAZO
+
+    // devolver Depósitos
+    function returnDeposits() public payable onlyOwner {
+        // ejecuta un revert si la subasta no ha finalizado.
+        if (block.timestamp < finishTimestamp) {
+            revert("The auction has not ended");
+        }
     }
 
     // mostrar Ganador
-    // mostrar Ofertas
-    // devolver Depósitos
-    // REEMBOLSO PARCIAL
-    // EXTENSION DEL PLAZO
+    function showWinner() external auctionFinished {}
 }
