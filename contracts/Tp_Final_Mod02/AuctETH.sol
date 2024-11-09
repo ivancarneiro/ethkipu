3f5576bf2382032b608503af0e49e1e646a134bc
ivancarneiro <ivanexequielc@gmail.com>
2024-11-08 23:37:49 -0300

    makeBid, showBids x2 => OK

--- contracts/Tp_Final_Mod02/AuctETH.sol
+++ contracts/Tp_Final_Mod02/AuctETH.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.8.2 <0.9.0;
+pragma solidity >= 0.8.2 < 0.9.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/utils/Strings.sol";
 
 /// @title AuctETH - A smart contract for Ethereum-based auctions
 /// @notice This contract allows users to participate in a timed auction
@@ -8,19 +10,21 @@ pragma solidity >=0.8.2 <0.9.0;
 /// @author Ivan Carneiro
 
 contract AuctETH {
-    address owner;
-    uint256 initValue;
-    uint256 currentBid;
-    uint256 bidIncrement = 2;
-    uint256 duration;
-    uint256 finishTimestamp;
+    // Contract variables
+    address public owner; // Contract owner's address
+    address public winner; // Auction winner's address
+    uint256 public initValue; // Initial auction value
+    uint256 public currentBid; // Current bid value
+    uint256 public bidIncrement; // Minimum bid increment (percentage)
+    uint256 public duration; // Auction duration (seconds)
+    uint256 public finishTimestamp; // Auction end timestamp
 
     /// @notice Constructor to initialize the auction.
     /// @param _initValue The initial value of the auction item.
-    /// @param _bidIncrement The minimum bid increment (in percentage), if not specified, will be 2%.
-    /// @param _duration The duration of the auction (in seconds), e.g. (5min = 300sec; 10min = 600sec; 1h = 3600sec; 1d = 86400sec).
+    /// @param _bidIncrement The minimum bid increment (in percentage).
+    /// @param _duration The duration of the auction (in seconds).
     constructor(uint256 _initValue, uint256 _bidIncrement, uint256 _duration) {
-        require(_initValue > 0,"The initial value must be greater than zero for the gas");
+        require(_initValue > 0, "The initial value must be greater than zero for gas");
         owner = msg.sender;
         initValue = _initValue;
         currentBid = _initValue;
@@ -29,24 +33,28 @@ contract AuctETH {
         finishTimestamp = block.timestamp + duration;
     }
 
-    mapping(address => uint256) public offers;
-
-    // struct Bidder (address)
+    // Data structures
     struct Bidder {
-        address addrs;
-        uint256 lastBid;
+        address addrs; // Bidder's Ethereum address
+        uint256 lastBid; // Bidder's last bid value
+        uint256 balance; // Bidder's balance
     }
 
-    Bidder[] public bidders; // array de todos los pujadores
+    struct Bid {
+        address bidder; // Bidder's Ethereum address
+        uint256 amount; // Bid value
+    }
 
-    address[] public uniqueAddr; // array de direcciones unicas
+    // Mappings and arrays
+    mapping(address => Bidder) public bidders; // Mapping of bidders by address
+    address[] public bidderAddresses; // Array of bidder addresses
 
-
-    event NewBid(address indexed _bidder, uint256 _bidValue);
+    // Events
+    event NewBid(address indexed bidder, uint256 bidValue); // Emitted when a new bid is placed
 
     // Modifiers
     modifier onlyOwner() {
-        require(msg.sender == owner,"Only the auction owner can execute this function");
+        require(msg.sender == owner, "Only the auction owner can execute this function");
         _;
     }
 
@@ -56,51 +64,85 @@ contract AuctETH {
     }
 
     modifier auctionFinished() {
-        require(block.timestamp >= finishTimestamp,"The auction has not yet ended");
+        require(block.timestamp >= finishTimestamp, "The auction has not yet ended");
         _;
     }
 
-
-    // mostrar oferta actual
-    function showCurrentBid() external view returns (uint256 _currentBid) {
-        _currentBid = currentBid;
-        return (_currentBid);
+    /// @notice Returns the current bid value.
+    /// @return The current bid value.
+    function showCurrentBid() external view returns (uint256) {
+        return currentBid;
     }
 
-    // ofertar
+    /// @notice Returns the minimum bid increment percentage.
+    /// @return The minimum bid increment percentage.
+    function incrementalPercentage() external view returns (uint256) {
+        return bidIncrement;
+    }
+
+    /// @notice Places a new bid.
+    /// @dev Requires bid value to exceed current bid by minimum increment.
     function makeBid() external payable auctionOpened {
-        address _bidder = msg.sender;
-        uint256 _bidValue = msg.value;
+        address bidder = msg.sender;
+        uint256 bidValue = msg.value;
 
-        require(_bidValue > (currentBid * ((bidIncrement + 100) / 100)),"The bid must exceed the best offer, taking into account the minimum incremental value");
-        if (offers[_bidder]==0) {
-            uniqueAddr.push(_bidder);
+        require(bidValue > (currentBid * (bidIncrement + 100) / 100), "The bid must outbid the best bid, considering the minimum increase percentage");
+
+        // Extend auction duration if bid is placed near end
+        if (block.timestamp >= finishTimestamp - 600) {
+            finishTimestamp += 600;
         }
-        offers[_bidder] += _bidValue;
-        currentBid = _bidValue;
-        bidders.push(Bidder(_bidder, _bidValue));
-        emit NewBid(_bidder, _bidValue);
+
+        // Update bidder's information
+        if (bidders[bidder].addrs != address(0)) {
+            bidders[bidder].lastBid = bidValue;
+            bidders[bidder].balance += bidValue;
+        } else {
+            bidderAddresses.push(bidder);
+            bidders[bidder] = Bidder(bidder, bidValue, bidValue);
+        }
+
+        winner = bidder;
+        currentBid = bidValue;
+
+        emit NewBid(bidder, bidValue);
     }
 
-    // mostrar ofertas
-    function showBidders() external view returns (Bidder[] memory _bidders) {
-        uint256 len = uniqueAddr.length;
-        for (uint256 i=0; i<len; i++){
-            _bidders[i] = Bidder( uniqueAddr[i],offers[uniqueAddr[i]]);
+    /// @notice Returns an array of all bids.
+    /// @return An array of Bid structs.
+    function showBids() public view returns (Bid[] memory) {
+        Bid[] memory bids = new Bid[](bidderAddresses.length);
+
+        for (uint256 i = 0; i < bidderAddresses.length; i++) {
+            bids[i] = Bid(bidderAddresses[i], bidders[bidderAddresses[i]].lastBid);
         }
+
+        return bids;
     }
 
-    // REEMBOLSO PARCIAL
-    // EXTENSION DEL PLAZO
+    /// @notice Alternative function to show offers and bids on JSON format
+    /// @notice Returns a JSON string of all bids.
+    /// @return A JSON string.
+    function getBids() public view returns (string memory) {
+        // Initializes the result string
+        string memory result = "[";
+ 
+        // Iterates over bidder addresses
+        for (uint256 i = 0; i < bidderAddresses.length; i++) {
+            // Constructs the JSON object for each bid
+            result = string(abi.encodePacked(result, "{\"bidder\":\"", Strings.toHexString(bidderAddresses[i]), "\",\"amount\":\"", Strings.toString(bidders[bidderAddresses[i]].lastBid), "\"}"));
 
-    // devolver Depósitos
-    function returnDeposits() public payable onlyOwner {
-        // ejecuta un revert si la subasta no ha finalizado.
-        if (block.timestamp < finishTimestamp) {
-            revert("The auction has not ended");
+            // Appends comma if not the last element
+            if (i < bidderAddresses.length - 1) {
+                result = string(abi.encodePacked(result, ","));
+            }
         }
-    }
 
-    // mostrar Ganador
-    function showWinner() external auctionFinished {}
+        // Closes the JSON string
+        result = string(abi.encodePacked(result, "]"));
+
+        // Returns the JSON string
+        return result;
+        }
+
 }
