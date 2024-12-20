9d25002c9d0c788a74df89580df9de90d9b9148b
Ivan Carneiro <64872710+ivancarneiro@users.noreply.github.com>
2024-12-20 10:26:09 -0300

    Update HackingGradeMe.sol

--- contracts/Tp_Final_Mod05_Hackin2024/HackingGradeMe.sol
+++ contracts/Tp_Final_Mod05_Hackin2024/HackingGradeMe.sol
@@ -10,23 +10,22 @@ interface IGrader {
 }
 
 // Pasos para obtener 100 puntos.
-// 1. Desplegar el contrato Haking con la dirección del contrato publicado por ETH.Kipu 0x6F14A55C82600DCAFeb1841243a15921DCD10aF7
+// 1. Desplegar el contrato HakingGradeMe pasando como parámetro la dirección del contrato publicado por ETH.Kipu 0x6F14A55C82600DCAFeb1841243a15921DCD10aF7
 
-// 2. Una vez desplegado HakingGradeMe, desde el contrato llamar a la funcion executeRetrieve2() dos veces, 
-// en cada llamada no te olvides de enviarle 5 wei. Esto incrementara el contador del contrato en 2, lo que te permitirá mintear un NFT.
+// 2. Una vez desplegado HakingGradeMe, desde el contrato llamar a la funcion executeRetrieve2() DOS VECES, en cada llamada NO TE OLVIDES DE ENVIAR 5 Wei. 
+// Esto incrementara el contador del contrato en 2, lo que te permitirá mintear un NFT.
 
 // 3. Llamar a la funcion executeMint() pasando la dirección de tu wallet como parámetro para que reciba el NFT. 
-// esto hará que el balanceOf de la wallet sea , requisito indispensable para luego poder llamar a graderMe. PERO NO TE APRESURES...
+// Esto hará que el balanceOf de la wallet sea 1, requisito indispensable para luego poder llamar a graderMe con esa Wallet directamente desde Hacking2024. PERO NO TE APRESURES...
 
-// 4. Todavía desde el contrato HackingGradeMe llamar a la funcion executeRetrieve2() pasando 5 wei, esto es CRUCIAL
-// para la variable "foo" de la wallet, con esto foo(tx.origin) == 1.
+// 4. Todavía desde el contrato HackingGradeMe, llamar a la funcion executeRetrieve() pasando 5 wei, 
+// esto es CRUCIAL para la variable "foo" de la wallet, con esto foo(tx.origin) == 1, otro requisito indispensable para luego poder llamar a graderMe.
 
 // 5. ÚLTIMO PASO. Ahora ve directamente al contrato publicado por ETH.Kipu https://sepolia.etherscan.io/token/0x6F14A55C82600DCAFeb1841243a15921DCD10aF7
 // o importalo desde RemixIDE e iteractua con la misma wallet.
-
-// a. Comprueba el valor de "foo" de tu wallet.
-// b. Comprueba el "balanecOf" de tu wallet.
-// Si cumples las condiciones ahora llama al gradeMe pasando tu nombre o aleas.
+    // a. Comprueba el valor de "foo" de tu wallet.
+    // b. Comprueba el "balanecOf" de tu wallet.
+    // Si cumples las condiciones ahora llama al gradeMe() pasando tu nombre o aleas.
 
 // FELICIDADES HAS OBTENIDO 100 PUNTOS!!!
 
